3c40914f326d5eea0dcc170cd0fc4e231a6fa4a9
sjon de gast <>
2019-02-18 18:06:47 +0100

    updated webpack.config.js file to take env.production to set mode and devtool, package.json updated npm scripts to build:dev and prod and setup to take new arguments for webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = {
+module.exports = (env) => {
+  return {
     entry: "./src/app.js",
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
     },
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"]
+        }
+      ]
     },
-    mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
+    mode: env.production ? "production" : "development",
+    devtool: env.production ? "source-map" : "cheap-module-eval-source-map",
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+      contentBase: path.join(__dirname, "public")
     }
+  };
 };
